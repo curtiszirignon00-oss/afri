ddb92bd7f2270e10b01d721ad0ddc43456e4d745
curtiszirignon00-oss <curtiszirignon00@gmail.com>
2025-11-03 20:42:10 +0100

    push after scraping change

--- backend/src/jobs/scraping.job.ts
+++ backend/src/jobs/scraping.job.ts
@@ -3,10 +3,10 @@ import { scrapeStock, scrapeIndex } from '../services/scraping.service';
 import { saveIndices } from '../services/index.service.prisma';
 import { saveStocks } from '../services/stock.service.prisma';
 
-// Tâche cron pour exécuter le scraping tous les jours à minuit
+// Tâche cron pour exécuter le scraping toutes les 10 heures
 
-cron.schedule('0 0 * * *', async () => { // Exécute à minuit chaque jour
-    console.log('Tâche de scraping exécutée tous les jours à minuit');
+cron.schedule('0 */10 * * *', async () => { // Exécute toutes les 10 heures
+    console.log('Tâche de scraping exécutée toutes les 10 heures');
     const stocks = await scrapeStock();
     const indices = await scrapeIndex();
     await saveStocks(stocks);
