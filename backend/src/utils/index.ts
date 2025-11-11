3cc96d9be2038d5ac1d71397505cb5a135c47be2
curtiszirignon00-oss <curtiszirignon00@gmail.com>
2025-11-11 11:28:44 +0100

    prof fix

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -26,13 +26,13 @@ export const getUserFromToken = async (req: Request) => {
     try {
         const isMobile = isUserOnMobile(req)
         const token = isMobile ? req.headers['authorization']?.split(' ')[1] : req.cookies.token
-        if(!token) createError.badRequest("Le token n'existe pas.")
+        if(!token) throw createError.badRequest("Le token n'existe pas.")
         const decoded = jwt.verify(token, config.jwt.secret);
         if (typeof decoded === "string" || !decoded) {
-            createError.badRequest("Le token est invalide.");
+            throw createError.badRequest("Le token est invalide.");
         }
         const user = await prisma.user.findUnique({where: {id: (decoded as DecodedToken).id}})
-        if(!user) createError.notFound("L'utilisateur n'existe pas.")
+        if(!user) throw createError.notFound("L'utilisateur n'existe pas.")
         return {user};
     } catch (error) {
         return {error}
