3ff2dd98f5b0151f07622b826f85cdc23ea8a7f9
curtiszirignon00-oss <curtiszirignon00@gmail.com>
2026-03-10 15:43:20 +0100

    s3

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express';
 import * as indexController from '../controllers/index.controller';
+import { admin } from '../middlewares/auth.middleware';
 
 const router = Router();
 
-router.post('/', indexController.createOrUpdateIndices);
+router.post('/', admin, indexController.createOrUpdateIndices);
 router.get('/', indexController.getIndices);
 router.get('/latest', indexController.getLatestIndices); // GET /api/indices/latest
 router.get('/history/:indexName', indexController.getIndexHistory); // GET /api/indices/history/:indexName?period=1Y
