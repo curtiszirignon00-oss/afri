cecb764bb84a10fb4a94997a874c54ff41065e3c
curtiszirignon00-oss <curtiszirignon00@gmail.com>
2026-03-12 16:18:02 +0100

    uniwax dashboard

---
+++ afribourse/src/pages/UniWaxDashboardPage.tsx
@@ -0,0 +1,9 @@
+export default function UniWaxDashboardPage() {
+  return (
+    <iframe
+      src="/UNIWAX_Dashboard_Analytique.html"
+      style={{ width: '100%', height: '100vh', border: 'none', display: 'block' }}
+      title="UNIWAX Dashboard Analytique"
+    />
+  );
+}
